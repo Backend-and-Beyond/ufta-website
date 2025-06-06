f1511c7514056e08bc9f8503006eb742c23dc3f5
dikamjit-borah <dikamjitborah.devacc01@gmail.com>
2025-06-06 15:59:02 +0530

    init gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `ufta-website`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-google-gtag"]
+};

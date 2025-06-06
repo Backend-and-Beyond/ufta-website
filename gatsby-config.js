19b039c09ddc84a26a6ce329e849f34e7b8636cd
dikamjit-borah <dikamjitborah.devacc01@gmail.com>
2025-06-06 16:02:45 +0530

    fix gtag issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,31 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `ufta-website`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `bibhuuniversalfitness`,
+    siteUrl: `https://www.bibhuuniversalfitness.com`,
   },
-  plugins: ["gatsby-plugin-google-gtag"]
+  plugins: [
+    {
+      resolve: "gatsby-plugin-google-gtag",
+      options: {
+        // add multiple tracking IDs and a pageview event will be fired for all of them.
+        trackingIds: ["G-XXXXXXXXXX"],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+        },
+      },
+    },
+  ],
 };

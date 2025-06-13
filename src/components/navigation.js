6ac5b0bfb49db0878588954c06fac33195f91f2a
dikamjit-borah <dikamjitborah.devacc01@gmail.com>
2025-06-13 16:06:43 +0530

    update home page

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -296,7 +296,7 @@ const Navigation = ({ location }) => {
               </motion.div>
             </motion.div>
             
-            {/* Sign Up button */}
+            {/* Get in Touch button */}
             <motion.div variants={navItemVariants}>
               <motion.div
                 whileHover={{ scale: 1.05 }}
@@ -304,7 +304,7 @@ const Navigation = ({ location }) => {
                 transition={{ duration: 0.2 }}
               >
                 <Link to="/signup" className="px-5 py-1.5 text-sm font-medium border border-[#00c8ff] text-white no-underline hover:bg-[#00c8ff]/10 transition-colors duration-200">
-                  Sign Up
+                  Get in Touch
                 </Link>
               </motion.div>
             </motion.div>
